042b9ff05923da486438f7b120e7113b631709fd
Ashwini <ashwinicloudpro@gmail.com>
2023-03-31 17:48:59 +0530

    first commit

---
+++ 1st file.py
@@ -0,0 +1,18 @@
+# my first Python Program 
+# i am interested
+
+print ("Hello World")
+print('congratulations for your 1st program')
+
+
+print('hello everyone',7)
+print(5)
+print('bye')
+
+print(2+13)
+
+print(2*13)
+
+print(2/13)
+
+print(2-13)
